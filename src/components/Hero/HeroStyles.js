ea3a36b385f17382ff445ac1b0553a8d3502afa2
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-12-18 12:57:55 -0800

    width fixes for large screens

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -3,6 +3,12 @@ import helloSplash from '../../imgs/helloSplash.svg';
 
 export const BackgroundHero = styled.div`
   height: 100%;
+  max-width: 1000px;
+  margin: auto;
+  @media (min-width: ${({ theme }) => theme.breakpoints.large}) {
+    border-radius: 10px;
+    box-shadow: 10px 10px 13px #00000070, -10px -10px 13px #3a3a3a70;
+  }
   background-image: url(${helloSplash});
   background-size: 260%;
   background-position-x: 95%;
