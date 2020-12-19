7f58b06963097c23139d2de1603446816520c018
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-12-18 17:51:16 -0800

    Intro UI changes on larger screens

--- src/components/Introduction/IntroductionStyles.js
+++ src/components/Introduction/IntroductionStyles.js
@@ -2,19 +2,34 @@ import styled from 'styled-components';
 
 export const StyledIntroduction = styled.div`
   max-width: 500px;
-  padding: 0 1rem;
-  margin: auto;
-  h3 {
-    font-size: 1.5rem;
-    line-height: 1.75rem;
-    color: ${({ theme }) => theme.color.tertiary};
+  margin: 0 auto;
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    max-width: 720px;
+    flex-direction: row;
+    justify-content: space-between;
   }
-  p {
-    font-size: ${({ theme }) => theme.fontSizes.normal};
-    text-align: justify;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div {
+    h3 {
+      font-size: 1.5rem;
+      line-height: 1.75rem;
+      color: ${({ theme }) => theme.color.tertiary};
+    }
   }
-  svg {
-    box-shadow: 10px 10px 13px #00000070, -10px -10px 13px #3a3a3a70;
-    border-radius: 100px;
+  div {
+    p {
+      font-size: ${({ theme }) => theme.fontSizes.normal};
+      text-align: justify;
+      padding: 0 1rem;
+      @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        padding-left: 3rem;
+      }
+    }
+    svg {
+      box-shadow: 10px 10px 13px #00000070, -10px -10px 13px #3a3a3a70;
+      border-radius: 100px;
+    }
   }
 `;
