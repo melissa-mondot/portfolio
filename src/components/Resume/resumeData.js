05f50500dce8b3b38ea7067ca373ea6d0d4a281a
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-07-02 10:50:00 -0700

    Add resume object

---
+++ src/components/Resume/resumeData.js
@@ -0,0 +1,60 @@
+export const resumeData = {
+  summary:
+    'Experienced web developer (8 years static web, 1+ year with React/Node), with a solid background in marketing and business development. Demonstrated ability to increase businesses’ online presence by creating creative websites and ensuring the availability of required IT services. Instrumental in producing new concepts and ideas for the achievement of set targets without compromising quality. Familiar with state management and developing/using REST APIs, experienced with UI libraries like react-strap, react-spring, and MUI. Skilled in CSS, animations, and graphic design. Well-versed in achieving business, branding, and marketing objectives using development tools and best practices along with professional relationship building with clients, stakeholders, and multi-disciplinary functions. ',
+  expertise: [
+    'Full Stack Web Development',
+    'UI Design &amp; Graphic Art',
+    'SCSS &amp; CSS3',
+    'Concept Creation',
+    'Project Management',
+    'Inbound Marketing',
+    'SEO &amp; Context Writing',
+    'Leadership &amp; Training',
+  ],
+  experience: [
+    {
+      company: 'Various Clients, SF Bay Area',
+      title: 'Freelance Web Developer for Small Businesses',
+      date: '2016-present',
+      description:
+        'Assess clients’ business needs and provide web-based solutions such as static web development, customizing SquareSpace templates, graphic designing, and SEO and content writing. Initiate inbound marketing as part of brand development, increasing the market presence of the business. Ensured timely creation and expansion of online visibility for over five clients.',
+      highlights: [
+        'Ensured timely creation and expansion of online visibility for over five clients.',
+      ],
+    },
+    {
+      company: 'Lambda School, San Francisco',
+      title: 'Section Lead, Full Stack Web',
+      date: '2019-2021',
+      description:
+        'Direct teams of 4-15 team leaders (TLs), while providing support in communication and scheduling from external associates. Facilitate students and TLs to develop soft and coding skills. Deliver guidance to up to 150 students on full stack web development program. Improve student’s performance outcomes by coordinating section instructions and student success initiatives. Devise new coaching ideas from statistics based on data points and students’ feedback.',
+      highlights: [
+        'Successfully guided multiple cohorts through Full Stack Web Development and Computer Science programs',
+      ],
+    },
+    {
+      company: 'GLI NorCal Landscape Construction, Greenbrae',
+      title: 'Business Development Officer',
+      date: '2012-2019',
+      description:
+        'Carried out branding with associated in/outbound marketing, managing web development, and increasing web presence. Ensured availability of required IT services. Developed custom micro-sites and landing pages for lead capture.',
+      highlights: [
+        'Played a key role as general idea creator to promote the brand and achieve set milestones.',
+        'Resurfaced the company after the great recession from 2012 to 2015 via re-branding and rebooting of the company.',
+      ],
+    },
+  ],
+  technicalProficiencies: [
+    'HTML/CSS3',
+    'JavaScript',
+    'React',
+    'Nodejs',
+    'Adobe Creative Suite',
+    'Figma/Whimsical',
+    'Google Ads/Analytics',
+  ],
+  education: [
+    'Computer Science Certificate, Lambda School, San Francisco, 2019',
+    'Bachelor of Arts (Graduated Magna Cum Laude), California State University, Sacramento',
+  ],
+};
